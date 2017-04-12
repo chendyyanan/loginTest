f2a0367b9bccd5580209b4b92dd8dd276d0b2f4b
Ethan <87382980@qq.com>
2017-04-12 18:50:22 +0800

    -a

---
+++ loginTest/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  loginTest
+//
+//  Created by Mr. Chen on 2017/4/12.
+//  Copyright © 2017年 liezhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Person : UILabel
+
+@end
---
+++ loginTest/Person.m
@@ -0,0 +1,21 @@
+//
+//  Person.m
+//  loginTest
+//
+//  Created by Mr. Chen on 2017/4/12.
+//  Copyright © 2017年 liezhong. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
