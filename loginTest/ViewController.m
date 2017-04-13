1eaef30d4f9b78942eb021314ec1f7881e432e8e
六年 <87382980@qq.com>
2017-04-13 11:05:03 +0800

    aaa

--- loginTest/ViewController.m
+++ loginTest/ViewController.m
@@ -28,8 +28,6 @@
     [self.view addSubview:testV];
     
     
-    //测试 pull
-    
     return;
     
     drawing = [[DrawingView alloc]initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, self.view.frame.size.height - 64)];
